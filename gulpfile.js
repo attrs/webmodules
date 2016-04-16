f0fdedb1edbbb0529c7f31edf49a799a99e20400
joje <joje.attrs@gmail.com>
2016-04-16 15:17:01 +0900

    Add gulp build / runtime.min.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,28 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var mocha = require('gulp-mocha');
+'use strict';
 
-var path = require('path');
-var fs = require('fs');
+const path = require('path');
+const gulp = require('gulp');
+const gutil = require('gulp-util');
+const rename = require("gulp-rename");
+const uglify = require('gulp-uglify');
+const rimraf = require('gulp-rimraf');
 
+const dist = path.join(__dirname);
+
+gulp.task('build.js.clean', () => {
+  return gulp.src('runtime.min.js', { read: false })
+    .pipe(rimraf());
+});
+
+gulp.task('build', ['build.js.clean'], () => {
+  return gulp.src('runtime.js')
+    .pipe(uglify())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest(dist));
+});
+
+// conclusion
+gulp.task('watch', ['build.watch']);
+gulp.task('default', ['build']);
