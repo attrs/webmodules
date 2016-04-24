4ad908bba5a95cdc1f73551e80fa197bf4612445
joje <joje.attrs@gmail.com>
2016-04-24 23:59:30 +0900

    ...

--- examples/bower/app.js
+++
@@ -1,6 +0,0 @@
-var path = require('path');
-
-console.log('dirname', __dirname);
-console.log('filename', __filename);
-console.log('path', path.join(__dirname, 'test'));
-console.log('hello', require('./hello.es6'));
