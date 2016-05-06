9b53d2a83485722dada1383d09b9cd094ef9cdd3
joje <joje.attrs@gmail.com>
2016-05-06 18:40:10 +0900

    Update example: es2015

--- examples/es2015/js/app.js
+++ examples/es2015/js/app.js
@@ -1,8 +1,9 @@
 import $ from 'jquery';
+import calc, {sayHello} from './sub.js';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/jumbotron.css';
 
-$(document).ready(() => {
-  console.log('DOM is ready');
+$(document).ready(($) => {
+  $('.jumbotron > .container > h1').html(`${sayHello()} - ${calc(1)}`);
 });
---
+++ examples/es2015/js/sub.js
@@ -0,0 +1,7 @@
+const getMessage = () => "Hello World";
+
+export var e = 2.71828182846;
+export var sayHello = getMessage;
+export default function(x) {
+  return Math.exp(x);
+}
