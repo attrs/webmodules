9aa1db6878ecc96bc374a8e048748890194be916
joje <joje.attrs@gmail.com>
2016-05-06 18:27:29 +0900

    Add example: react-dom

---
+++ examples/react-dom/js/app.jsx
@@ -0,0 +1,21 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Avatar = require('./avatar.jsx');
+var AvatarES2015 = require('./avatar.es2015.jsx').Avatar;
+
+window.addEventListener('DOMContentLoaded', function() {
+  ReactDOM.render(
+    React.createElement(Avatar, {
+      pagename: 'Engineering'
+    }),
+    document.getElementById('react')
+  );
+  
+  ReactDOM.render(
+    React.createElement(AvatarES2015, {
+      pagename: 'Engineering'
+    }),
+    document.getElementById('react-es2015')
+  );
+});
