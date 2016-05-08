a81dfced15929f0ea552c1f7ef237ea75d563750
joje <joje.attrs@gmail.com>
2016-05-08 13:42:42 +0900

    Update bundle dependencies

--- examples/commonjs/js/sub/index.js
+++ examples/commonjs/js/sub/index.js
@@ -1,1 +1,16 @@
+console.info('sub/index.js');
+console.log('__dirname', __dirname);
+console.log('__filename', __filename);
+console.log('module', module);
+console.log('process', process);
+console.log('global', global);
+console.log('Buffer', Buffer);
+console.log('setTimeout', setTimeout);
+console.log('clearTimeout', clearTimeout);
+console.log('setInterval', setInterval);
+console.log('clearInterval', clearInterval);
+console.log('setImmediate', setImmediate);
+console.log('clearImmediate', clearImmediate);
+
+
 module.exports = 'CommonJS';
