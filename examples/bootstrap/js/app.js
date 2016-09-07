4ac39c069e5082fa4cb141237282762f528443ec
joje <joje.attrs@gmail.com>
2016-09-07 11:09:42 +0900

    ..

--- examples/bootstrap/js/app.js
+++ examples/bootstrap/js/app.js
@@ -1,7 +1,7 @@
 var $ = require('jquery');
 
 require('bootstrap');
-require('bootstrap/less/bootstrap.less');
-require('bootstrap/less/theme.less');
+require('bootstrap/dist/css/bootstrap.min.css');
+require('bootstrap/dist/css/bootstrap-theme.min.css');
 require('../css/theme.css');
 require('./docs.min.js');
